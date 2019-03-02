db44186086a09716cfb16114b983fa53ffbbc592
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-03-02 11:10:59 -0600

    updated js

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('hello');
+//zoe ames
+//brittney spears
+//tori amos
+// fName= 'Saul';
+// lName = 'Solano';
+// const firstName = 'Zoe';
+// const lastName = 'Smith';
+// // console.log(firstName + ' ' + lastName);
+// console.log(`${firstName} ${lastName}`);
+// const firstName1 = 'Britney';
+// const lastName1 = 'Spears';
+
+// console.log(`${firstName1} ${lastName1}`);
+
+// const firstName2 = 'Tori';
+// const lastName2 = 'Amos';
+// console.log(`${firstName2} ${lastName2}`);
+
+const namePrinter = (firstName, lastName) => {
+    console.log(`${firstName} ${lastName}`);
+};
+
+namePrinter("Zoe", "Ames");
